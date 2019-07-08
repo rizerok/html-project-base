065471674aa6f4eea819c755a21737d1e50233c9
Alexey Bashinsky <alexey.bashinsky@actimind.com>
2019-07-08 05:11:52 +0300

    build: babel split webpack configs, optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,100 +1,76 @@
-const { resolve } = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+/* eslint global-require: "off", no-console: "off" */
+const merge = require('webpack-merge');
+const dotenv = require('dotenv');
 
-let devServer;
+dotenv.config();
 
-function htmlReloadPlugin() {
-    const cache = {};
-    const plugin = {name: 'CustomHtmlReloadPlugin'};
-    this.hooks.compilation.tap(plugin, compilation => {
-        compilation.hooks.htmlWebpackPluginAfterEmit.tap(plugin, data => {
-            const orig = cache[data.outputName];
-            const html = data.html.source();
-            // plugin seems to emit on any unrelated change?
-            if (orig && orig !== html) {
-                devServer.sockWrite(devServer.sockets, 'content-changed');
-            }
-            cache[data.outputName] = html;
-        });
-    });
-}
+const sameRule = (a, b) => {
+    if (String(a.test) !== String(b.test)) { // check test
+        return false;
+    }
 
-const config = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: "index.dist.js",
-        path: resolve('public')
-    },
-    resolve: {
-        alias: {
-            styles: resolve('src','assets','styles'),
-            img:resolve('src','assets','images')
+    let use1;
+    let use2;
+    // eslint-disable-next-line no-unused-expressions
+    Array.isArray(a.use) ? use1 = a.use : use1 = [a.use];
+    // eslint-disable-next-line no-unused-expressions
+    Array.isArray(b.use) ? use2 = b.use : use2 = [b.use];
+    if (use1.length !== use2.length) { // check loaders count
+        return false;
+    }
+
+    for (let i = 0; i < use1.length; i++) { // check by loaders
+        if (use1[i].loader !== use2[i].loader) {
+            return false;
         }
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        before(app, server) {
-            devServer = server;
-        },
-        // open: true,
-        port: 8000,
-        contentBase: './public',
-        writeToDisk: true,
-        index: 'index.html',
-        hot: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                include: resolve('src', 'assets', 'styles'),
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true,
-                            importLoaders: 2,
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true,
-                        }
-                    },
-                    {
-                        loader: 'sass-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                include: resolve('src', 'assets', 'images'),
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.pug$/,
-                use: [
-                    'pug-loader'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: 'index.pug'
-        }),
-        // new webpack.HotModuleReplacementPlugin()
-        htmlReloadPlugin
-    ]
+    }
+
+    return true;
 };
 
-module.exports = config;
+const mergeConfig = {
+    customizeArray(a, b, key) {
+        if (key === 'module.rules') {
+            const rules = b;
+            a.forEach(elA => {
+                if (rules.every(r => !sameRule(r, elA))) { // uniq
+                    rules.push(elA);
+                }
+            });
+            return rules;
+        }
+        return undefined;
+    },
+    customizeObject(a, b, key) {
+        if (key === 'entry') {
+            return b;
+        }
+        return undefined;
+    }
+};
+
+module.exports = env => {
+    const ENV = env || process.env.NODE_ENV || 'development';
+
+    let clientConfig;
+
+    console.log(`Run ${ENV} build.`);
+
+    // eslint-disable-next-line default-case
+    switch (ENV) {
+    case 'development': {
+        const base = require('./webpack/webpack.client.js');
+        const dev = require('./webpack/webpack.dev.js');
+        clientConfig = merge(mergeConfig)(base, dev);
+        break;
+    }
+    case 'production': {
+        const base = require('./webpack/webpack.client.js');
+        const prod = require('./webpack/webpack.prod.js');
+        clientConfig = merge(mergeConfig)(base, prod);
+        break;
+    }
+    }
+
+    return clientConfig;
+};
